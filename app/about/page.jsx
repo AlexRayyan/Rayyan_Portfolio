88afdb2b15afac7733f899485e4a8dcd38812958
Salahuddin3676 <salahuddin3676@gmail.com>
2024-03-03 12:44:23 +0530

    new change

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="flex items-center justify-center w-full min-h-screen text-5xl font-bold ">
-      About
+      About Page
     </div>
   );
 };
